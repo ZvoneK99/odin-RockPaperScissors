7a8f62cda462bfc116b5bb9ae97847cfee8b4656
Zvone <zvone.kozull1999@gmail.com>
2024-05-09 23:38:29 +0200

    Trenutno igra funkcionira

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
+let korisnikPobjede = 0;
+let računaloPobjede = 0; 
+let nerjeseno = 0;
+
+//funkcija za slučajni odabir od strane računala
 function getComputerChoice(){
-    let pobjednik;
-    let randomBroj= Math.floor(Math.random()*3)
-        if(randomBroj===0){
-            pobjednik = "Kamen";
-        }
-        else if(randomBroj===1) {
-            pobjednik = "Škare";
-        }
-        else if(randomBroj===2) {
-            pobjednik = "Papir";
-        }
-        return pobjednik;
-    }
+ 
+    const choices = ["kamen", "škare", "papir"];
+    const randomBroj = Math.floor(Math.random() * 3);
+    return choices[randomBroj];
+}
+
+let unosRačunala = getComputerChoice();
+document.write(`Odabir računala je: ${unosRačunala.charAt(0).toUpperCase() + unosRačunala.slice(1)}` + "<br>");
+
+//funkcija za odabir od strane osobe
+function getHumanChoice() {
+
+    let korisnikUnos = prompt("Upišite želite li: Kamen, Škare ili Papir");
+    korisnikUnos = korisnikUnos.toLowerCase();
     
-document.write(getComputerChoice())
+    if (korisnikUnos === "kamen" || korisnikUnos === "škare" || korisnikUnos === "papir") {
+        document.write("Korisnikov unos je: " + korisnikUnos.charAt(0).toUpperCase() + korisnikUnos.slice(1) + "<br>");
+        return korisnikUnos;
+    } else {
+        return getHumanChoice();
+    }
+}
+
+let korisnikovUnos = getHumanChoice();
+
+//funkcija koja izvršava igru
+function playRound(unosRačunala, korisnikovUnos) {
+  
+    if (unosRačunala === korisnikovUnos) {
+        window.alert("Nerješeno je.");
+    } 
+    else if ((unosRačunala === "škare" && korisnikovUnos === "papir") ||
+               (unosRačunala === "papir" && korisnikovUnos === "kamen") ||
+               (unosRačunala === "kamen" && korisnikovUnos === "škare")) {
+        window.alert("Računalo pobjeđuje.");
+    } 
+    else {
+        window.alert("Korisnik pobjeđuje.");
+    }
+}
+
+playRound(unosRačunala, korisnikovUnos);
